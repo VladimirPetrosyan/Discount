5333d97baccaa89877b42c1cdf9c244bd69b1acd
VladimirPetrosyan <petrosyan412@icloud.com>
2021-10-19 18:59:15 +0300

    Добавьте файлы проекта.

---
+++ Discount/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Discount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a > 500)
+            {
+                b = (a * 3) / 100;
+                a = a - b;
+                Console.WriteLine(a);
+              }
+            if (a < 500)
+            {
+                Console.WriteLine(a);
+            }
+                if (a > 1500) ;
+            { 
+                b = (a * 5) / 100;
+                a = a - b;
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
